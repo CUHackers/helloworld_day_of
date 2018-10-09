6685fbab40e75b1d669a3acab66f6078bd0d5e4d
Charlie Gallentine <chgallentine@gmail.com>
2018-10-09 19:46:49 -0400

    Wrong start date

--- js/clock.js
+++ js/clock.js
@@ -2,12 +2,12 @@
 * @Author: Charlie Gallentine
 * @Date:   2018-10-05 15:34:33
 * @Last Modified by:   Charlie Gallentine
-* @Last Modified time: 2018-10-09 19:45:56
+* @Last Modified time: 2018-10-09 19:46:39
 */
 
 const year = 2018;
 const month = 9;
-const day = 9;
+const day = 13;
 const eventStartTime = 8;
 const eventEndTime = 20;
 
